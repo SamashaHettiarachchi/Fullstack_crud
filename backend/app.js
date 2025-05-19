c3cb0c212d34e1c4e3f77935705129c3d7d14241
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-05-19 19:55:08 +0530

    change app.js

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/userRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
+const cors = require("cors");
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // Route middlewar
 app.use(express.json());
+app.use(cors());
 app.use("/users", userRoutes);
 
-
 // Connect to MongoDB and start server
-mongoose.connect("mongodb+srv://admin:nWaQilnkuSrXjXFw@cluster0.ajk3e.mongodb.net/test?retryWrites=true&w=majority")
+mongoose
+  .connect(
+    "mongodb+srv://admin:nWaQilnkuSrXjXFw@cluster0.ajk3e.mongodb.net/test?retryWrites=true&w=majority"
+  )
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(3000, () => {
-      console.log("Server running on port 3000");
+    app.listen(5000, () => {
+      console.log("Server running on port 5000");
     });
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
