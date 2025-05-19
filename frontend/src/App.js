a096f1100561d5b01b87cce0bcb538df64a57af2
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-05-19 19:53:44 +0530

    change app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import './App.css';
-import Home from './components/Home/Home';
-import User from './components/Add User/AddUser';
-import Userdetails from './components/User Details/UserDetails';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Home from "./components/Home/Home";
+import User from "./components/Add User/AddUser";
+import Userdetails from "./components/User Details/UserDetails";
+import { Routes, Route } from "react-router-dom";
+import UpdateUsers from "./components/UpdateUser/updateUser";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/mainhome" element={<Home />} />
         <Route path="/adduser" element={<User />} />
         <Route path="/userdetails" element={<Userdetails />} />
+        <Route path="/update/:id" element={<UpdateUsers />} />
       </Routes>
     </div>
   );
