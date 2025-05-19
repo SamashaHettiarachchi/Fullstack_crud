a4f9ec50831309b7ac21610c050504bf1ff1d6c7
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-05-19 19:54:40 +0530

    change user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const user= require('../model/userModel');
+const user = require("../model/userModel");
 
-const userController = require('../controllers/userContrallers');
+const userController = require("../controllers/userContrallers");
 
-router.get('/', userController.getAllUsers);
-router.post('/', userController.addUsers); 
-router.get('/:id', userController.getById);      // Get all users
-router.put('/:id', userController.updateUser); // Update user by ID
-router.delete('/:id', userController.deleteUser); // Delete user by ID
+router.get("/", userController.getAllUsers);
+router.post("/", userController.addUsers);
+router.get("/:id", userController.getById); // Get all users
+router.put("/:id", userController.updateUser); // Update user by ID
+router.delete("/:id", userController.deleteUser); // Delete user by ID
 
 module.exports = router; // Export the router
